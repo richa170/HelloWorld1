52fe865658cd324a787dde1286b84e69da8fa432
Deepti Arya <DA11316@deeptis-mbp.attlocal.net>
2015-12-13 10:37:29 -0600

    Initial Commit

---
+++ MyHelloWorldProdcut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyHelloWorldProdcut
+//
+//  Created by Deepti Arya on 12/13/15.
+//  Copyright (c) 2015 MyHelloWorldOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
